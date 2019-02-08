a25f266a9ce70ac4036385108df210559801638b
Ebran Khan <ebrank97@gmail.com>
2019-02-08 23:09:51 +0530

    Display Error Handling in register from

--- validation/register.js
+++ validation/register.js
@@ -29,16 +29,16 @@ module.exports = function validateRegisterInput(data) {
     errors.password = 'Password must be at least 6 characters';
   }
 
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
+  }
+
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password field is required';
   }
 
   if (Validator.isEmpty(data.password2)) {
     errors.password2 = 'Confirm Password field is required';
-  }
-
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = 'Passwords must match';
   }
 
   return {
