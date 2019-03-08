976532ef35a8bf47afb89cd8e5899820b66fe01e
Ebran Khan <ebrank97@gmail.com>
2019-03-08 13:05:27 +0530

    Profile By handle Sub Components

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,9 @@ import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
-import Profiles from './components/Profiles/Profiles';
+
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 import store from './store';
 
@@ -59,20 +61,37 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/add-experience" component={AddExperience} />
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/add-education" component={AddEducation} />
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
               </Switch>
             </div>
             <Footer />
